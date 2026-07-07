e6d74537662a76a0e61a0ac6f14b82fe946ac138
Bhoomi Chowksey <149859840+bhoomichowksey@users.noreply.github.com>
2026-07-08 00:06:14 +0530

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -4,11 +4,15 @@ import { NextRequest, NextResponse } from 'next/server'
 export async function GET(request: NextRequest) {
   const { searchParams, origin } = request.nextUrl
   const code = searchParams.get('code')
+
+  // Redirect user to homepage after successful verification
   const next = searchParams.get('next') ?? '/'
 
   if (code) {
     const supabase = await createClient()
+
     const { error } = await supabase.auth.exchangeCodeForSession(code)
+
     if (!error) {
       return NextResponse.redirect(`${origin}${next}`)
     }
